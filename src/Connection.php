1fe5b7ebdad6aa4b5f88e61b2db2ac7b9153fc1c
Oleg V <olegv@semalt.com>
2019-06-19 18:46:15 +0300

    Initial commit

---
+++ src/Connection.php
@@ -0,0 +1,271 @@
+<?php
+
+namespace DBWrapper;
+
+class Connection
+{
+    const FETCH_ASSOC = 'FETCH_ASSOC';
+    const FETCH_NUM = 'FETCH_NUM';
+    const FETCH_OBJ = 'FETCH_OBJ';
+    const ATTR_DEFAULT_FETCH_MODE = 'ATTR_DEFAULT_FETCH_MODE';
+	const ATTR_CAMEL_CASE = 'ATTR_CAMEL_CASE';
+
+	const FIELD_TINYINT = 1;
+	const FIELD_SMALLINT = 2;
+	const FIELD_MEDIUMINT = 9;
+	const FIELD_INTEGER = 3;
+	const FIELD_BIGINT = 8;
+	const FIELD_FLOAT = 4;
+	const FIELD_DOUBLE = 5;
+	const FIELD_DECIMAL = 246;
+
+    private $conn;
+	private $connectError;
+    private $attrs = array();
+    private $lastQuery;
+    private $reseller = null;
+    private $host = null;
+
+    private $beforeQueryCallback = null;
+    private $afterQueryCallback = null;
+
+    private static $conns = [];
+
+    public function __construct($host, $user, $pass, $db, $opts = [])
+	{
+		$this->host = $host;
+        $connKey = $db.'*'.$host;
+        if (isset(self::$conns[$connKey]) && empty($opts['forceNew'])) {
+            $this->conn = self::$conns[$connKey];
+        } else {
+            $this->conn = mysqli_connect($host, $user, $pass, $db);
+			$this->connectError = mysqli_connect_error();
+			if ($this->connectError) return;
+
+			self::$conns[$connKey] = $this->conn;
+			mysqli_query($this->conn, 'SET NAMES utf8');
+        }
+    }
+
+    public function query($query)
+	{
+        $args = func_get_args();
+        if (count($args) > 1) {
+            $query = call_user_func_array(array($this, 'prepareQuery'), $args);
+        }
+
+		if (is_callable($this->beforeQueryCallback)) {
+			$fn = $this->beforeQueryCallback;
+			$newQuery = $fn($query);
+			if ($newQuery) {
+				$query = $newQuery;
+			}
+		}
+
+        $this->lastQuery = $query;
+
+        $start = microtime(true);
+        $res = mysqli_query($this->conn, $query);
+		$end = microtime(true);
+
+		$time = $end - $start;
+
+		if (is_callable($this->afterQueryCallback)) {
+			$fn = $this->afterQueryCallback;
+			$fn($this, $res, $time);
+		}
+
+        return new QueryResult($this->conn, $res, $this->attrs);
+    }
+
+	public function onBeforeQuery($fn)
+	{
+		$this->beforeQueryCallback = $fn;
+	}
+
+	public function onAfterQuery($fn)
+	{
+		$this->afterQueryCallback = $fn;
+	}
+
+    public function exec($query)
+	{
+        return $this->query($query);
+    }
+
+    public function quote($raw)
+	{
+        return "'".mysqli_real_escape_string($this->conn, $raw)."'";
+    }
+
+	public function quoteArray($array)
+	{
+		$newArray = [];
+		foreach ($array as $elem) {
+			$newArray[] = $this->quote($elem);
+		}
+		return $newArray;
+	}
+
+    public function escape($raw)
+	{
+        return mysqli_real_escape_string($this->conn, $raw);
+    }
+
+	public function escapeWord($raw)
+	{
+		$param = preg_replace('~[^a-zA-Z0-9_.-]~', '', $raw);
+		$param = '`'.$param.'`';
+        return $param;
+    }
+
+    public function affectedRows()
+	{
+        return mysqli_affected_rows($this->conn);
+    }
+
+    public function escapeLike($raw)
+	{
+        $param = mysqli_real_escape_string($this->conn, $raw);
+        $param = str_replace(array('_', '%'), array('\_', '\%'), $param);
+        return $param;
+    }
+
+	/* alias to lastInsertId */
+	public function insertId()
+	{
+		return $this->lastInsertId();
+	}
+
+    public function lastInsertId()
+	{
+        return mysqli_insert_id($this->conn);
+    }
+
+    public function startTransaction()
+	{
+		$this->query('START TRANSACTION');
+	}
+
+	public function commit()
+	{
+		$this->query('COMMIT');
+	}
+
+	public function rollback()
+	{
+		$this->query('ROLLBACK');
+	}
+
+	public function getConnectError()
+	{
+		return $this->connectError;
+	}
+
+	public function ping()
+	{
+		return mysqli_ping($this->conn);
+	}
+
+    public function setAttribute($key, $value)
+	{
+        $this->attrs[$key] = $value;
+    }
+
+    public function getAttribute($key)
+	{
+        if (!isset($this->attrs[$key])) return null;
+        return $this->attrs[$key];
+    }
+
+    public function getLastError()
+	{
+        return mysqli_error($this->conn);
+    }
+
+	/* alias to getLastError */
+	public function error()
+	{
+        return $this->getLastError();
+    }
+
+    public function getLastQuery()
+	{
+        return $this->lastQuery;
+    }
+
+    /* alias to getLastQuery */
+    public function lastQuery()
+	{
+        return $this->getLastQuery();
+    }
+
+	public function close()
+	{
+		return mysqli_close($this->conn);
+	}
+
+    public function prepareQuery()
+	{
+        $args = func_get_args();
+		$rawQuery = $args[0];
+
+		if (count($args) <= 1) return $rawQuery;
+
+        if (is_array($args[1])) {
+            $params = $args[1];
+        } else {
+            $params = array_slice($args, 1);
+        }
+        $curIndex = 0;
+        $query = preg_replace_callback('/:[ifslw]\??/', function($m) use($params, &$curIndex) {
+            $placeholder = $m[0];
+			if (strpos($placeholder, '?') !== false) {
+				$placeholder = rtrim($placeholder, '?');
+				$nullable = true;
+			} else {
+				$nullable = false;
+			}
+
+            if (!isset($params[$curIndex])) {
+				$curIndex++;
+				if ($nullable) return 'NULL';
+				return $placeholder;
+			}
+
+			$param = $params[$curIndex++];
+            switch ($placeholder) {
+                case ':i': return intval($param);
+                case ':f': return floatval($param);
+                case ':s': return $this->quote($param);
+                case ':l': return $this->escapeLike($param);
+				case ':w': return $this->escapeWord($param);
+            }
+        }, $rawQuery);
+
+        return $query;
+    }
+
+	public function checkTableExists($tbl, $database = null)
+	{
+		if ($database) {
+			$query = $this->prepareQuery('SELECT 1 FROM :w.:w LIMIT 1', $database, $tbl);
+		} else {
+			$query = $this->prepareQuery('SELECT 1 FROM :w LIMIT 1', $tbl);
+		}
+		@$this->query($query);
+		if ($this->error()) return false;
+		return true;
+	}
+
+	/* alias to checkTableExists */
+	public function tableExists($tbl, $database = null)
+	{
+		return $this->checkTableExists($tbl, $database);
+	}
+
+	public function getHost()
+	{
+		return $this->host;
+	}
+}
---
+++ src/QueryResult.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace App\Util\MySQLi;
+
+class QueryResult implements \Iterator
+{
+	private $caller;
+    private $conn;
+    private $res;
+    private $attrs;
+    private $position = -1;
+    private $currentRow;
+	
+	private static $intTypes = [
+		Connection::FIELD_TINYINT,
+		Connection::FIELD_SMALLINT,
+		Connection::FIELD_MEDIUMINT,
+		Connection::FIELD_INTEGER,
+		Connection::FIELD_BIGINT
+	];
+	
+	private static $floatTypes = [
+		Connection::FIELD_FLOAT,
+		Connection::FIELD_DOUBLE,
+		Connection::FIELD_DECIMAL
+	];
+
+    public function __construct($conn, $res, $attrs = [], $caller = null)
+	{
+        $this->conn = $conn;
+        $this->res = $res;
+        $this->attrs = $attrs;
+        $this->caller = $caller;
+    }
+
+    public function fetchColumn($colIndex = 0)
+	{
+        if (!$this->res || mysqli_num_rows($this->res) < 1) return null;
+        return mysqli_fetch_row($this->res)[$colIndex];
+    }
+
+    public function fetch($mode = null)
+	{
+        if ($this->res === false) return null;
+
+        $mode = $this->transformMode($mode);
+
+        $this->position++;
+        $row = mysqli_fetch_assoc($this->res);
+        $this->currentRow = !empty($row) ? $this->transformRow($row, $mode) : false;
+        return $this->currentRow;
+    }
+
+    public function fetchObject()
+	{
+        return $this->fetch(Connection::FETCH_OBJ);
+    }
+
+    public function fetchAssoc()
+	{
+        return $this->fetch(Connection::FETCH_ASSOC);
+    }
+
+    public function fetchNum()
+	{
+        return $this->fetch(Connection::FETCH_NUM);
+    }
+
+    public function fetchAll($mode = null)
+	{
+        if ($this->res === false) return null;
+
+        $mode = $this->transformMode($mode);
+
+        $rows = array();
+        while ($row = mysqli_fetch_assoc($this->res))
+		{
+            $rows[] = $this->transformRow($row, $mode);
+        }
+        return $rows;
+    }
+
+    public function fetchAllCol($col = 0)
+	{
+        if ($this->res === false) return null;
+
+        $arr = array();
+        if (is_int($col)) {
+            while ($row = mysqli_fetch_row($this->res)) {
+                $arr[] = $row[$col];
+            }
+        } else {
+            while ($row = mysqli_fetch_assoc($this->res)) {
+                $arr[] = $row[$col];
+            }
+        }
+
+        return $arr;
+    }
+
+    public function rowCount()
+	{
+        if ($this->res === false) return null;
+        if ($this->res === true) return mysqli_affected_rows($this->conn);
+        return mysqli_num_rows($this->res);
+    }
+	
+	public function id()
+	{
+		return mysqli_insert_id($this->conn);
+	}
+	
+	public function error()
+	{
+		return mysqli_error($this->conn);
+	}
+
+    public function rewind()
+	{
+        $this->position = -1;
+        $this->next();
+    }
+
+    public function current()
+	{
+        return $this->currentRow;
+    }
+
+    public function key()
+	{
+        return $this->position;
+    }
+
+    public function next()
+	{
+        $this->currentRow = $this->fetch();
+    }
+
+    public function valid()
+	{
+        return !empty($this->currentRow);
+    }
+
+    public function lastQuery()
+	{
+		return $this->caller->getLastQuery();
+	}
+	
+	private function camelize($raw)
+	{
+		return preg_replace_callback('~_([a-zA-Z])~', function($match) {
+			return strtoupper($match[1]);
+		}, trim($raw, '_'));
+	}
+
+    private function transformRow($row, $mode)
+	{
+		$fields = mysqli_fetch_fields($this->res);
+
+		$shouldCamelize = !empty($this->attrs[Connection::ATTR_CAMEL_CASE]);
+
+		$newRow = [];
+		$i = 0;
+		foreach ($row as $key => $val) {
+			if ($shouldCamelize) {
+				$key = $this->camelize($key);
+			}
+
+			$fieldType = $fields[$i]->type;
+			if (in_array($fieldType, self::$intTypes) && $val !== null) {
+				$val = intval($val);
+			}
+			if (in_array($fieldType, self::$floatTypes) && $val !== null) {
+				$val = floatval($val);
+			}
+
+			$newRow[$key] = $val;
+			$i++;
+		}
+		$row = $newRow;
+		unset($newRow);
+
+        switch ($mode) {
+            case Connection::FETCH_ASSOC:	return $row;
+            case Connection::FETCH_OBJ:		return (object)$row;
+            case Connection::FETCH_NUM:		return array_values($row);
+        }
+        return null;
+    }
+
+    private function transformMode($mode)
+	{
+        if ($mode !== null) {
+            return $mode;
+        }
+        if (isset($this->attrs[Connection::ATTR_DEFAULT_FETCH_MODE])) {
+            return $this->attrs[Connection::ATTR_DEFAULT_FETCH_MODE];
+        }
+        return Connection::FETCH_OBJ;
+    }
+}
